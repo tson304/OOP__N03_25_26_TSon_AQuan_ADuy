dca0506d3e2c1da5597a2c12c8120b98495bf9c0
anhquan <lquankk@gmail.com>
2025-09-04 07:52:30 +0000

    private class BaiHat

--- QuanLyNhac/model/BaiHat.java
+++ QuanLyNhac/model/BaiHat.java
@@ -1,9 +1,11 @@
+
+
 public class BaiHat {
-    public int id;
-    public String tenBaiHat;
-    public String tenCaSi;
-    public int thoiLuong;
-    public String theLoai;
+    private int id;
+    private String tenBaiHat;
+    private String tenCaSi;
+    private int thoiLuong;
+    private String theLoai;
 
     public BaiHat (int id, String tenBaiHat,String tenCaSi,int thoiLuong,String theLoai){
         this.id=id;
@@ -13,16 +15,47 @@ public class BaiHat {
         this.theLoai = theLoai;
         
     }
+        public int getId() {
+        return id;
+    }
+
+    public String getTenBaiHat() {
+        return tenBaiHat;
+    }
+
+    public String getTenCaSi() {
+        return tenCaSi;
+    }
+
+    public int getThoiLuong() {
+        return thoiLuong;
+    }
+
+    public String getTheLoai() {
+        return theLoai;
+    }
+    public void setTenBaiHat(String tenBaiHat) {
+        this.tenBaiHat = tenBaiHat;
+    }
+
+    public void setTenCaSi(String tenCaSi) {
+        this.tenCaSi = tenCaSi;
+    }
+
+    public void setThoiLuong(int thoiLuong) {
+        this.thoiLuong = thoiLuong;
+    }
+
+    public void setTheLoai(String theLoai) {
+        this.theLoai = theLoai;
+    }
     public void hienThiThongTin() {
         System.out.println("Tên bài hát:"+ tenBaiHat);
         System.out.println("Tên người hat:"+ tenCaSi);
         System.out.println("Thể loại:"+ theLoai);
         System.out.println("Thời Lượng:"+ thoiLuong + "giây");
     }
-    @Override
-    public String toString(){
-        return id + ":" +tenBaiHat + "-" + tenCaSi + "(" + theLoai +","+ thoiLuong + "s )";
-        }
+
 }
 
 
