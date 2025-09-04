d116a650eb74c9a2cd15c153a47ac4995bc7d1de
Lê Anh Duy <23010710@st.phenikaa-uni.edu.vn>
2025-09-04 15:25:31 +0700

    Update NguoiDung.java

--- QuanLyNhac/model/NguoiDung.java
+++ QuanLyNhac/model/NguoiDung.java
@@ -3,35 +3,41 @@ public class NguoiDung {
     private String ngaySinh;
     private String gioiTinh;
 
-    public NguoiDung (String tenNguoiDung, String ngaySinh; String gioiTinh){
+ 
+    public NguoiDung(String tenNguoiDung, String ngaySinh, String gioiTinh) {
         this.tenNguoiDung = tenNguoiDung;
         this.ngaySinh = ngaySinh;
-        this.gioiTinh = gioiTinh;   
-    }
-    public String getTenNguoiDung(){
-        return tenNguoiDung;
-    }
-    public void setTennguoidung (String tenNguoiDung){
-        this.tenNguoiDung = tenNguoiDung;
-    }
-    public String getNgaySinh(){
-        return ngaySinh;
-    }
-    public void setNgaySinh(String ngaySinh){
-        this.ngaySinh = ngaySinh;
-    }
-    public String getGioiTinh(){
-        return gioiTinh;
-    }
-    public void setGioiTinh(String gioiTinh){
         this.gioiTinh = gioiTinh;
     }
-    public void hienthiThongTin(){
-        System.out.println("Ten nguoi dung: " + tenNguoiDung);
-        System.out.println("Ngay sinh: " + ngaySinh);
-        System.out.println("Gioi tinh: " + gioiTinh);
+
+
+    public String getTenNguoiDung() {
+        return tenNguoiDung;
     }
 
+    public void setTenNguoiDung(String tenNguoiDung) {
+        this.tenNguoiDung = tenNguoiDung;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Tên người dùng: " + tenNguoiDung);
+        System.out.println("Ngày sinh: " + ngaySinh);
+        System.out.println("Giới tính: " + gioiTinh);
+    }
 }
-
-
