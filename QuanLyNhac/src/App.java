bef4f2a6b416a6c5d4aefed3fa04d7b1d65f9b80
tson304 <149238613+tson304@users.noreply.github.com>
2025-09-05 16:03:31 +0700

    Add files via upload

--- QuanLyNhac/src/App.java
+++ QuanLyNhac/src/App.java
@@ -2,7 +2,25 @@ import model.thuVienNhac;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        thuVienNhac baiHat1 = new thuVienNhac("Son Tung MTP", "Pop", "Dung lam trai tim anh dau");
+        thuVienNhac baiHat2 = new thuVienNhac("Son Tung MTP", "Pop", "Chung ta cua hien tai");
+        thuVienNhac baiHat3 = new thuVienNhac("Mono", "Pop", "Cham hoa");
+
+        System.out.println(baiHat1.getBaiHat());
+        System.out.println(baiHat1.getCaSi());
+        System.out.println(baiHat1.getTheLoai());
+
+        System.out.println("------------------------------");
+
+        System.out.println(baiHat2.getBaiHat());
+        System.out.println(baiHat2.getCaSi());
+        System.out.println(baiHat2.getTheLoai());
+
+        System.out.println("------------------------------");
+
+        System.out.println(baiHat3.getBaiHat());
+        System.out.println(baiHat3.getCaSi());
+        System.out.println(baiHat3.getTheLoai());
+
     }
 }
-
