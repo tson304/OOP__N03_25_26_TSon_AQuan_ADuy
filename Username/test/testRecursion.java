254d52beca99ea3e349c0a61dfa6cb8ad972349f
anhquan <lquankk@gmail.com>
2025-09-05 11:10:08 +0000

    sua file recursion

--- Username/test/testRecursion.java
+++ Username/test/testRecursion.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class testRecursion{
     public static int fibo(int n){
-        if (n==0) return 0;
-        if (n==1) return 1;
+        if(n<=1) return n;
         return fibo(n-1)+fibo(n-2);
 
 
@@ -16,7 +15,10 @@ public class testRecursion{
         Scanner sc= new Scanner(System.in);
         System.err.println("Nhap so tu nhien:");
         n=sc.nextInt();
-        System.err.println("Fibonacci tu 1 den "+ n +" la: " +fibo(n));
+        System.err.println("Fibonacci tu 1 den "+ n +" la: ");
+         for (int i = 0; i <= n; i++) {
+            System.out.print(fibo(i) + " ");
+        }
         sc.close();
 
     }
