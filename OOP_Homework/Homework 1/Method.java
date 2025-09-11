d4d22629cc510fbbb5f4e326cee1ea6fb2752569
tson304 <149238613+tson304@users.noreply.github.com>
2025-09-11 12:27:54 +0700

    Homework 1 commited

---
+++ OOP_Homework/Homework 1/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+
+    static void myMethod() {
+        System.out.println("Xin chao, toi vua duoc khoi tao!");
+    }
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
