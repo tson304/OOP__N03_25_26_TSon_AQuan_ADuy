01393388fd31db7474d77dc1b1a9d8f31ab7b401
anhquan7826335 <lquankk@gmail.com>
2025-09-19 17:33:02 +0700

    chuc nang nhap tu phim

---
+++ Username/src/LayDuLieu.java
@@ -0,0 +1,34 @@
+package Username.src;
+
+import java.util.Scanner;
+
+public class LayDuLieu {
+    private Scanner sc;
+    public LayDuLieu() {
+        sc = new Scanner(System.in);
+    }   
+    public int laySoNguyen(String thongBao) {
+        System.out.print(thongBao);
+        int so = sc.nextInt();
+        sc.nextLine(); 
+        return so;
+    }
+    public String layChuoi(String thongBao) {
+        System.out.print(thongBao);
+        return sc.nextLine();
+    }
+    public double laySoThuc(String thongBao) {
+        System.out.print(thongBao);
+        double so = sc.nextDouble();
+        sc.nextLine(); 
+        return so;
+    }
+    public BaiHat layBaiHat() {
+        int id = laySoNguyen("Nhap id: ");
+        String tenBaiHat = layChuoi("Nhap ten bai hat: ");
+        String tenCaSi = layChuoi("Nhap ten ca si: ");
+        int thoiLuong = laySoNguyen("Nhap thoi luong (giay): ");
+        String theLoai = layChuoi("Nhap the loai: ");
+        return new BaiHat(id, tenBaiHat, tenCaSi, thoiLuong, theLoai);
+    }
+}
