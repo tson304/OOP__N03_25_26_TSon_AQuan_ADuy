2e09dd6203d3c9066af0fbb2f39ef89cec628ffa
anhquan7826335 <lquankk@gmail.com>
2025-09-19 17:23:30 +0700

    them chuc nang xoa

---
+++ Username/test/testXoaBaiHat.java
@@ -0,0 +1,20 @@
+package Username.test;
+
+import Username.src.BaiHat;
+import Username.src.DsBaiHat;
+import Username.src.XoaBaiHat;
+
+public class testXoaBaiHat {
+    public static void main(String[] args) {  
+        DsBaiHat ds = new DsBaiHat();
+        System.out.println("So bai hat trong ds:"+ds.getSoLuongBaiHat()); 
+    
+        XoaBaiHat xoa = new XoaBaiHat(ds);
+        xoa.xoaBaiHatTheoId(2);
+        System.out.println("So bai hat trong ds:"+ds.getSoLuongBaiHat()); 
+        xoa.xoaBaiHatTheoTen("buong doi tay nhau ra");
+        
+        
+    }
+}
+
