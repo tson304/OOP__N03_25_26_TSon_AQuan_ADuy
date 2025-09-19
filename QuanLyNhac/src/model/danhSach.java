5916d0673e2a689f736a151bd0078fa9b40fe0f1
tson304 <149238613+tson304@users.noreply.github.com>
2025-09-19 10:46:12 +0000

    update thuVienNhac

---
+++ QuanLyNhac/src/model/danhSach.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+public abstract class danhSach {
+    public void themPhanTu() {}
+    public void xoaPhanTu() {}
+}
---
+++ QuanLyNhac/src/model/danhSachNguoiDung.java
@@ -0,0 +1,3 @@
+public class danhSachNguoiDung extends danhSach {
+    
+}
