909d4e0ed2a580653495867cc8ae4e7b415295ca
anhquan7826335 <lquankk@gmail.com>
2025-09-19 16:17:13 +0700

    ds Bai hat

---
+++ Username/src/DsBaiHat.java
@@ -0,0 +1,10 @@
+package Username.src;
+
+public class DsBaiHat {
+    private BaiHat[] ds;
+    private int soLuong;
+    public DsBaiHat() {
+        ds = new BaiHat[100]; // Giả sử tối đa 100 bài hát
+        soLuong = 0;
+    }
+}
