d37ba3731b28caa578f13e7eefb8931da534423e
tson304 <149238613+tson304@users.noreply.github.com>
2025-09-26 10:13:51 +0000

    update practice 6

--- QuanLyNhac/test/testDogAnimal.java
+++
@@ -1,9 +0,0 @@
-public class testDogAnimal {
-    public static void DogAnimal() {
-        Dog a = new Dog();
-        a.animalSound();
-        a.walk();
-        a.wagging();
-        a.breed();
-    }
-}
