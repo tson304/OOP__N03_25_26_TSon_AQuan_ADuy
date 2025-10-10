90d23a901356f6b4c423521690b94fe2bec10a96
tson304 <149238613+tson304@users.noreply.github.com>
2025-10-10 15:50:38 +0700

    Delete QuanLyNhac directory

--- QuanLyNhac/src/Recursion.java
+++
@@ -1,8 +0,0 @@
-public class Recursion {
-    static void f(Object[] x, int i) {
-        if (i < x.length) {
-            System.out.println(x[i]);
-            f(x, i + 1);
-        }
-    }
-}
--- QuanLyNhac/test/testRecursion.java
+++
@@ -1,8 +0,0 @@
-public class testRecursion {
-    public static void Recursion() {
-        System.out.println("RECURSION CALL");
-        Recursion.f(new Object[] {"one", "two", "three"}, 0);
-        Recursion.f(new Object[] {1, "two", 3.5}, 0);
-        Recursion.f(new Object[] {"one", new Recursion(), 3.5}, 0);
-    }
-}
