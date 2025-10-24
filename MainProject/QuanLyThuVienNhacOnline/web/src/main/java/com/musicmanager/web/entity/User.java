8a266a540ec272474db5454aac3ba87cafcf634c
Lê Anh Duy <23010710@st.phenikaa-uni.edu.vn>
2025-10-24 16:44:51 +0000

    Chinh sua user

--- MainProject/QuanLyThuVienNhacOnline/web/src/main/java/com/musicmanager/web/entity/User.java
+++ MainProject/QuanLyThuVienNhacOnline/web/src/main/java/com/musicmanager/web/entity/User.java
@@ -1,55 +1,61 @@
 package com.musicmanager.web.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-
     private String id;
-    private String name;
-    private String email;
-    private String password;
 
-    public String getId()
-    {
+    private String username;
+    private String email;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String username, String email, String role) {
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getId() {
         return id;
     }
 
-    public void setId(String id)
-    {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getName()
-    {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name)
-    {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getEmail()
-    {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email)
-    {
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword()
-    {
-        return password;
+    public String getRole() {
+        return role;
     }
 
-    public void setPassword(String password)
-    {
-        this.password = password;
+    public void setRole(String role) {
+        this.role = role;
     }
 }
