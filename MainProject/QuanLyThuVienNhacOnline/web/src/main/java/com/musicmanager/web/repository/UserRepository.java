d1c324fa880342d7f464c8b39ca3c2542535c589
Lê Anh Duy <23010710@st.phenikaa-uni.edu.vn>
2025-10-24 16:45:43 +0000

    Chinh sua repository user

--- MainProject/QuanLyThuVienNhacOnline/web/src/main/java/com/musicmanager/web/repository/UserRepository.java
+++ MainProject/QuanLyThuVienNhacOnline/web/src/main/java/com/musicmanager/web/repository/UserRepository.java
@@ -4,5 +4,9 @@ import com.musicmanager.web.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, String>{}
+public interface UserRepository extends JpaRepository<User, String> {
+    List<User> findByUsernameContainingIgnoreCaseOrEmailContainingIgnoreCase(String username, String email);
+}
