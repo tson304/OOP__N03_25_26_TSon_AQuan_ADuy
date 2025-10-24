7433e31c94293b4ef98fae9b48d642828a7cfd0b
Lê Anh Duy <23010710@st.phenikaa-uni.edu.vn>
2025-10-24 16:42:26 +0000

    Chinh sua controller user

--- MainProject/QuanLyThuVienNhacOnline/web/src/main/java/com/musicmanager/web/controller/UserController.java
+++ MainProject/QuanLyThuVienNhacOnline/web/src/main/java/com/musicmanager/web/controller/UserController.java
@@ -3,45 +3,51 @@ package com.musicmanager.web.controller;
 import com.musicmanager.web.entity.User;
 import com.musicmanager.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/users")
-public class UserController
-{
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
     @Autowired
     private UserService userService;
 
-    @PostMapping
-    User createUser(@RequestBody User request)
-    {
-        return userService.createUser(request);
-    }
-
     @GetMapping
-    List<User> getUsers()
-    {
-        return userService.getUsers();
+    public String getUsers(Model model) {
+        List<User> users = userService.getUsers();
+        model.addAttribute("users", users);
+        model.addAttribute("userRequest", new User());
+        return "users";
     }
 
-    @GetMapping("/{id}")
-    User getUser(@PathVariable String id)
-    {
-        return userService.getUser(id);
+    @PostMapping("/create")
+    public String createUser(@ModelAttribute("userRequest") User user) {
+        userService.createUser(user);
+        return "redirect:/users";
     }
 
-    @PutMapping("/{id}")
-    User updateUser(@PathVariable String id, @RequestBody User request)
-    {
-        return userService.updateUser(id, request);
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable String id, @ModelAttribute("userRequest") User user) {
+        userService.updateUser(id, user);
+        return "redirect:/users";
     }
 
-    @DeleteMapping("/{id}")
-    String deleteUser(@PathVariable String id)
-    {
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable String id) {
         userService.deleteUser(id);
-        return "User has been deleted";
+        return "redirect:/users";
+    }
+
+    @GetMapping("/search")
+    public String searchUsers(@RequestParam("keyword") String keyword, Model model) {
+        List<User> users = userService.searchUsers(keyword);
+        model.addAttribute("users", users);
+        model.addAttribute("userRequest", new User());
+        model.addAttribute("searchQuery", keyword);
+        return "users";
     }
 }
