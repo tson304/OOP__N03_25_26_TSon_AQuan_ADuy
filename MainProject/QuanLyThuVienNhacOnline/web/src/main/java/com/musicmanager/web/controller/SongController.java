54badc40f81b1fc89ee8a43a8ce480c0ce5e7b52
anhquan <lquankk@gmail.com>
2025-11-03 06:07:45 +0000

    sua html va them test

--- MainProject/QuanLyThuVienNhacOnline/web/src/main/java/com/musicmanager/web/controller/SongController.java
+++ MainProject/QuanLyThuVienNhacOnline/web/src/main/java/com/musicmanager/web/controller/SongController.java
@@ -3,10 +3,13 @@ package com.musicmanager.web.controller;
 import com.musicmanager.web.entity.Song;
 import com.musicmanager.web.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -17,25 +20,25 @@ public class SongController {
     private SongService songService;
 
     @GetMapping
-public String listSongs(Model model) {
-    List<Song> songs = songService.getAllSongs();
-    model.addAttribute("songs", songs);
-    model.addAttribute("numberOfSongs", songs.size());
-    model.addAttribute("songRequest", new Song()); // 
-    return "songs"; // tên file HTML (songs.html)
-}
+    public String listSongs(Model model) {
+        List<Song> songs = songService.getAllSongs();
+        model.addAttribute("songs", songs);
+        model.addAttribute("numberOfSongs", songs.size());
+        model.addAttribute("songRequest", new Song()); // 
+        return "songs"; // tên file HTML (songs.html)
+    }
 
     // Tìm kiếm bài hát theo ten bai hat
     @GetMapping("/search")
-public String searchSongs(@RequestParam(value = "keyword", required = false) String keyword, Model model) {
-    List<Song> songs = songService.searchSongs(keyword);
+    public String searchSongs(@RequestParam(value = "keyword", required = false) String keyword, Model model) {
+        List<Song> songs = songService.searchSongs(keyword);
 
-    model.addAttribute("songs", songs);
-    model.addAttribute("searchQuery", keyword);
-    model.addAttribute("numberOfSongs", songs.size());
-    model.addAttribute("songRequest", new Song());
-    return "songs";
-}
+        model.addAttribute("songs", songs);
+        model.addAttribute("searchQuery", keyword);
+        model.addAttribute("numberOfSongs", songs.size());
+        model.addAttribute("songRequest", new Song());
+        return "songs";
+    }
 
     // Tạo mới bài hát
     @PostMapping("/create")
@@ -57,4 +60,6 @@ public String searchSongs(@RequestParam(value = "keyword", required = false) Str
         songService.deleteSong(id);
         return "redirect:/songs";
     }
+
+
 }
